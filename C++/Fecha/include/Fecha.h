8540fa85755facb556977d53c994c8b417e1181b
Ignacio Romero <nachoromero84@hotmail.com>
2022-06-15 02:15:40 -0300

    fecha

--- C++/Fecha/include/Fecha.h
+++ C++/Fecha/include/Fecha.h
@@ -1,19 +1,23 @@
 #ifndef FECHA_H
 #define FECHA_H
 
+#define FECHA_BASE 0
 
 class Fecha
 {
     public:
+        
         Fecha();
         Fecha(int dias,int mes,int anio);
 
-        Fecha sumarDias(int dias) const;
+        
         int difEnDias(const Fecha* otraFecha) const;
+
         int operator -(const Fecha* otraFecha) const;
 
     private:
         int diaRel;
+        int cantDiaMesAcum(int mes);
 
 };
 
--- C++/Fecha/src/Fecha.cpp
+++ C++/Fecha/src/Fecha.cpp
@@ -13,3 +13,8 @@ Fecha::Fecha(int dia,int mes,int anio){
 
 }
 
+int Fecha::cantDiaMesAcum(int mes){
+    static int diasAcum[12] = {0,31,59,90,120,151,181,212,243,273,304,334};
+
+    return diasAcum[mes];
+}
