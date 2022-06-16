8b89d5ceee9bb6092fbe92a34538e0a46cb7c3da
Ignacio Romero <nachoromero84@hotmail.com>
2022-06-16 09:12:31 -0300

    Fecha

--- C++/Fecha/include/Fecha.h
+++ C++/Fecha/include/Fecha.h
@@ -1,24 +1,42 @@
 #ifndef FECHA_H
 #define FECHA_H
 
+using namespace std;
+
+#include <iostream>
+
 #define FECHA_BASE 0
+
+
 
 class Fecha
 {
     public:
         
         Fecha();
-        Fecha(int dias,int mes,int anio);
+        Fecha(int dia,int mes,int anio);
 
+        void setDMA(int dia,int mes,int anio);
+        int getDiaRel() const;
+
+
+        int difEnDias(const Fecha& otraFecha) const;
+
+        int operator >(const Fecha& otraFecha) const;
+        int operator <(const Fecha& otraFecha) const;
+        int operator -(const Fecha& otraFecha) const;
         
-        int difEnDias(const Fecha* otraFecha) const;
-
-        int operator -(const Fecha* otraFecha) const;
-
+        bool esFechaValida(const Fecha& otraFecha) const;
+        static bool esFechaValida(int dia,int mes,int anio);
     private:
         int diaRel;
         int cantDiaMesAcum(int mes);
+        static int cantDiaMes(int mes,int anio);
+        
 
 };
+
+ostream& operator <<(ostream& os, const Fecha& otraFecha);
+istream& operator >>(istream& is, const Fecha& otraFecha);
 
 #endif // FECHA_H
--- C++/Fecha/src/Fecha.cpp
+++ C++/Fecha/src/Fecha.cpp
@@ -9,7 +9,29 @@ Fecha::Fecha(){
 
 Fecha::Fecha(int dia,int mes,int anio){
 
+    int diasAcum = anio*365 + anio/4 - anio/100 + anio/400 + cantDiaMesAcum(mes) + dia;
 
+    this->diaRel = diasAcum;
+
+}
+
+void Fecha::setDMA(int dia,int mes, int anio){
+
+    if(!esFechaValida(dia,mes,anio)){
+        throw FechaExc(dia + '/' + mes + '/' + anio + "no es una fecha valida");
+    }
+
+    int nuevoDiaAcum = anio*365 + anio/4 - anio/100 + anio/400 + cantDiaMesAcum(mes) + dia;
+
+    this->diaRel = nuevoDiaAcum;
+
+}
+
+bool Fecha::esFechaValida(int dia,int mes,int anio){
+
+    if(anio > 0){
+        if(mes > 1 && mes < 12)
+    }
 
 }
 
